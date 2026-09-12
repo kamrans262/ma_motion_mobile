4809c8db84a24509cd1e4b3370355a3e57505605
Kamran <41294454+kamrans262@users.noreply.github.com>
2026-09-13 00:14:05 +0500

    chore: remove unused typography import

--- lib/features/artwork_viewer/presentation/widgets/artwork_maker_info_page.dart
+++ lib/features/artwork_viewer/presentation/widgets/artwork_maker_info_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 
 import '../../../../core/theme/app_colors.dart';
-import '../../../../core/theme/app_text_styles.dart';
 import '../../../../core/widgets/ma_svg_asset.dart';
 import '../../data/maker_current_show_repository.dart';
 import '../../domain/artwork_detail.dart';
