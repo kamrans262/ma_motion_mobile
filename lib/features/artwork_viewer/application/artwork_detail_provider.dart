c27093b232627ec316b956d6281d2954bc50b422
Kamran <41294454+kamrans262@users.noreply.github.com>
2026-09-13 00:08:36 +0500

    fix: cache artwork detail during Maker navigation

--- lib/features/artwork_viewer/application/artwork_detail_provider.dart
+++ lib/features/artwork_viewer/application/artwork_detail_provider.dart
@@ -3,7 +3,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../data/artwork_detail_repository.dart';
 import '../domain/artwork_detail.dart';
 
-final artworkDetailProvider = FutureProvider.autoDispose
-    .family<ArtworkDetail, int>((ref, artworkId) {
-      return ref.watch(artworkDetailRepositoryProvider).fetch(artworkId);
-    });
+final artworkDetailProvider = FutureProvider.family<ArtworkDetail, int>((
+  ref,
+  artworkId,
+) {
+  return ref.watch(artworkDetailRepositoryProvider).fetch(artworkId);
+});
