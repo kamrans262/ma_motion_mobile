7cc223a37a871679453fef6a4413d2c22735b0e5
Kamran <41294454+kamrans262@users.noreply.github.com>
2026-09-13 02:38:57 +0500

    tune: use smoother MA splash timing

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MaMotionApp extends StatefulWidget {
   const MaMotionApp({
     super.key,
     this.splashAutoPlay = true,
-    this.splashDuration = const Duration(milliseconds: 5200),
+    this.splashDuration = const Duration(milliseconds: 6600),
   });
 
   final bool splashAutoPlay;
