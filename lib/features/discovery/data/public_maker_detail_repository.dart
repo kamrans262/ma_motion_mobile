1c9a06b51f24bc13df946925cf6ac43700c004ed
Kamran <41294454+kamrans262@users.noreply.github.com>
2026-09-13 00:09:16 +0500

    feat: add public Maker detail repository

---
+++ lib/features/discovery/data/public_maker_detail_repository.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../core/network/api_envelope.dart';
+import '../../../core/network/api_gateway.dart';
+import '../../../core/network/api_paths.dart';
+import '../../../core/providers/core_providers.dart';
+import '../domain/public_maker_detail.dart';
+
+final publicMakerDetailRepositoryProvider = Provider<PublicMakerDetailRepository>((ref) {
+  return PublicMakerDetailRepository(api: ref.watch(apiGatewayProvider));
+});
+
+final publicMakerDetailProvider = FutureProvider.family<PublicMakerDetail, int>((ref, makerId) {
+  return ref.watch(publicMakerDetailRepositoryProvider).fetch(makerId);
+});
+
+class PublicMakerDetailRepository {
+  const PublicMakerDetailRepository({required this.api});
+
+  final ApiGateway api;
+
+  Future<PublicMakerDetail> fetch(int makerId) async {
+    final response = await api.get(ApiPaths.maker(makerId), requiresAuth: false);
+    return PublicMakerDetail.fromMap(ApiEnvelope(raw: response).dataMap);
+  }
+}
