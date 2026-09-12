4ead95540a2596f6f6dfdf342201ca13f9ae96c3
Kamran <41294454+kamrans262@users.noreply.github.com>
2026-09-12 18:39:38 +0500

    style: normalize Hostinger API environment test

--- test/core/config/app_environment_test.dart
+++ test/core/config/app_environment_test.dart
@@ -2,15 +2,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:ma_motion_mobile/core/config/app_environment.dart';
 
 void main() {
-  test(
-    'default API uses the deployed Hostinger Laravel endpoint',
-    () {
-      expect(
-        AppEnvironment.apiBaseUrl,
-        'https://slategray-fly-111965.hostingersite.com/api/v1',
-      );
-      expect(AppEnvironment.connectTimeout.inSeconds, greaterThan(0));
-      expect(AppEnvironment.receiveTimeout.inSeconds, greaterThan(0));
-    },
-  );
+  test('default API uses the deployed Hostinger Laravel endpoint', () {
+    expect(
+      AppEnvironment.apiBaseUrl,
+      'https://slategray-fly-111965.hostingersite.com/api/v1',
+    );
+    expect(AppEnvironment.connectTimeout.inSeconds, greaterThan(0));
+    expect(AppEnvironment.receiveTimeout.inSeconds, greaterThan(0));
+  });
 }
