465a3eeb02b64dd9b82087ff6809b6a33b6e141e
Kamran <41294454+kamrans262@users.noreply.github.com>
2026-09-12 18:29:12 +0500

    test: format deployed API environment contract

--- test/core/config/app_environment_test.dart
+++ test/core/config/app_environment_test.dart
@@ -3,12 +3,12 @@ import 'package:ma_motion_mobile/core/config/app_environment.dart';
 
 void main() {
   test(
-    'unconfigured debug API uses the local Laravel development endpoint',
+    'default API uses the deployed Hostinger Laravel endpoint',
     () {
       expect(
-      AppEnvironment.apiBaseUrl,
-      'https://slategray-fly-111965.hostingersite.com/api/v1',
-    );
+        AppEnvironment.apiBaseUrl,
+        'https://slategray-fly-111965.hostingersite.com/api/v1',
+      );
       expect(AppEnvironment.connectTimeout.inSeconds, greaterThan(0));
       expect(AppEnvironment.receiveTimeout.inSeconds, greaterThan(0));
     },
