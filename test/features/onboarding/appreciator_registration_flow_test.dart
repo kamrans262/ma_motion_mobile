1a1ef8534acbb374e14f4976257ce3ba5f1a9f28
Kamran <41294454+kamrans262@users.noreply.github.com>
2026-09-14 22:34:02 +0500

    style: format onboarding footer assertions

--- test/features/onboarding/appreciator_registration_flow_test.dart
+++ test/features/onboarding/appreciator_registration_flow_test.dart
@@ -127,10 +127,14 @@ void main() {
         final lastWidget = find.byKey(Key(entry.value));
 
         expect(footer, findsOneWidget);
-        expect(find.byKey(const Key('maker_next_button')).hitTestable(),
-            findsOneWidget);
-        expect(find.byKey(const Key('maker_back_button')).hitTestable(),
-            findsOneWidget);
+        expect(
+          find.byKey(const Key('maker_next_button')).hitTestable(),
+          findsOneWidget,
+        );
+        expect(
+          find.byKey(const Key('maker_back_button')).hitTestable(),
+          findsOneWidget,
+        );
         expect(find.byKey(const Key('maker_step_dot_0')), findsOneWidget);
 
         final lastWidgetRect = tester.getRect(lastWidget);
