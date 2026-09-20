17942816422b1e39a2aacf8c9fbac57ca36229de
Kamran <41294454+kamrans262@users.noreply.github.com>
2026-09-20 22:09:55 +0500

    test: expect updated 32px Helvetica Neue onboarding heading and -0.3 tracking [skip ci]

--- test/features/onboarding/appreciator_registration_flow_test.dart
+++ test/features/onboarding/appreciator_registration_flow_test.dart
@@ -52,7 +52,8 @@ void main() {
     final heading = tester.widget<Text>(
       find.byKey(const Key('maker_step_heading')),
     );
-    expect(heading.style?.fontSize, 30);
+    expect(heading.style?.fontSize, 32);
+    expect(heading.style?.letterSpacing, -0.3);
     expect(heading.style?.fontWeight, FontWeight.w500);
 
     for (var index = 0; index < 3; index++) {
