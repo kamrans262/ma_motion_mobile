4ed7ab565552c324dd4f138041ad4a4020978934
Kamran <41294454+kamrans262@users.noreply.github.com>
2026-09-22 22:24:59 +0500

    fix(settings): import auth repository provider from core providers [skip ci]

--- lib/features/settings/presentation/screens/maker_info_settings_screen.dart
+++ lib/features/settings/presentation/screens/maker_info_settings_screen.dart
@@ -7,10 +7,10 @@ import 'package:image_picker/image_picker.dart';
 import 'package:video_player/video_player.dart';
 
 import '../../../../core/network/api_exception.dart';
+import '../../../../core/providers/core_providers.dart';
 import '../../../../core/theme/app_button_styles.dart';
 import '../../../../core/theme/app_colors.dart';
 import '../../../../core/theme/app_text_styles.dart';
-import '../../../auth/data/auth_repository.dart';
 import '../../../auth/data/experience_switch_repository.dart';
 import '../../../auth/domain/maker_entry_destination.dart';
 import '../../../onboarding/presentation/widgets/ma_choice_chip.dart';
