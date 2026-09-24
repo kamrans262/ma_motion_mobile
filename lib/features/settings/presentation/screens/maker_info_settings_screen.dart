69dd6b019c34875fa8df3121f50538cf29340727
Kamran <41294454+kamrans262@users.noreply.github.com>
2026-09-24 22:33:27 +0500

    fix(ui): apply optical text offset to actual non-OTP input baselines

--- lib/features/settings/presentation/screens/maker_info_settings_screen.dart
+++ lib/features/settings/presentation/screens/maker_info_settings_screen.dart
@@ -1029,7 +1029,7 @@ class _LabeledField extends StatelessWidget {
             keyboardType: keyboardType,
             minLines: minLines,
             maxLines: maxLines,
-            textAlignVertical: TextAlignVertical.center,
+            textAlignVertical: TextAlignVertical.top,
             style: AppTextStyles.field,
             cursorColor: AppColors.primary,
             decoration: InputDecoration(
@@ -1416,7 +1416,7 @@ class _CarouselEditor extends StatelessWidget {
             key: Key('maker_settings_artwork_title_$slot'),
             controller: titleController,
             maxLength: 180,
-            textAlignVertical: TextAlignVertical.center,
+            textAlignVertical: TextAlignVertical.top,
             style: AppTextStyles.field,
             cursorColor: AppColors.primary,
             decoration: InputDecoration(
@@ -1450,7 +1450,7 @@ class _CarouselEditor extends StatelessWidget {
           key: Key('maker_settings_carousel_caption_$slot'),
           controller: captionController,
           maxLength: 280,
-          textAlignVertical: TextAlignVertical.center,
+          textAlignVertical: TextAlignVertical.top,
           style: AppTextStyles.field,
           cursorColor: AppColors.primary,
           decoration: InputDecoration(
