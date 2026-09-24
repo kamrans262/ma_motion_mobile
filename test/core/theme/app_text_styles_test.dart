b6c5c6afa3c225eec77e91ff512d7c5f2af6a8f5
Kamran <41294454+kamrans262@users.noreply.github.com>
2026-09-25 00:29:05 +0500

    style(test): format bundled Helvetica typography assertions

--- test/core/theme/app_text_styles_test.dart
+++ test/core/theme/app_text_styles_test.dart
@@ -5,21 +5,26 @@ import 'package:ma_motion_mobile/core/theme/app_text_styles.dart';
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  test('the supplied Helvetica file is bundled with updated typography', () async {
-    final font = await rootBundle.load('assets/fonts/HelveticaNeueLTStd-Md.otf');
-    expect(font.lengthInBytes, 28260);
+  test(
+    'the supplied Helvetica file is bundled with updated typography',
+    () async {
+      final font = await rootBundle.load(
+        'assets/fonts/HelveticaNeueLTStd-Md.otf',
+      );
+      expect(font.lengthInBytes, 28260);
 
-    expect(AppTextStyles.onboardingHeading.height, 1.18);
-    expect(AppTextStyles.onboardingHeading.letterSpacing, 0);
-    expect(AppTextStyles.onboardingHelper.height, 1.4);
-    expect(AppTextStyles.onboardingHelper.letterSpacing, 0.2);
-    expect(AppTextStyles.field.height, 1.25);
-    expect(AppTextStyles.field.letterSpacing, 0.2);
-    expect(AppTextStyles.fieldHint.height, 1.25);
-    expect(AppTextStyles.fieldHint.letterSpacing, 0.2);
-    expect(AppTextStyles.buttonDark.height, 1.2);
-    expect(AppTextStyles.buttonPurple.letterSpacing, 0.2);
-    expect(AppTextStyles.chip.height, 1.25);
-    expect(AppTextStyles.error.height, 1.3);
-  });
+      expect(AppTextStyles.onboardingHeading.height, 1.18);
+      expect(AppTextStyles.onboardingHeading.letterSpacing, 0);
+      expect(AppTextStyles.onboardingHelper.height, 1.4);
+      expect(AppTextStyles.onboardingHelper.letterSpacing, 0.2);
+      expect(AppTextStyles.field.height, 1.25);
+      expect(AppTextStyles.field.letterSpacing, 0.2);
+      expect(AppTextStyles.fieldHint.height, 1.25);
+      expect(AppTextStyles.fieldHint.letterSpacing, 0.2);
+      expect(AppTextStyles.buttonDark.height, 1.2);
+      expect(AppTextStyles.buttonPurple.letterSpacing, 0.2);
+      expect(AppTextStyles.chip.height, 1.25);
+      expect(AppTextStyles.error.height, 1.3);
+    },
+  );
 }
