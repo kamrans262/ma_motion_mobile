57cdfa17c360b99d5d6c50237b9eee330a1502b6
Kamran <41294454+kamrans262@users.noreply.github.com>
2026-09-26 21:03:53 +0500

    style(inputs): set Appreciator bottom padding to 10 [skip ci]

--- lib/features/settings/presentation/widgets/appreciator_settings_modal.dart
+++ lib/features/settings/presentation/widgets/appreciator_settings_modal.dart
@@ -644,7 +644,7 @@ class _SettingsField extends StatelessWidget {
             errorStyle: AppTextStyles.error.copyWith(fontSize: 14),
             filled: true,
             fillColor: AppColors.filterInputFill,
-            contentPadding: const EdgeInsets.fromLTRB(12, 16, 12, 12),
+            contentPadding: const EdgeInsets.fromLTRB(12, 16, 12, 10),
             enabledBorder: OutlineInputBorder(
               borderRadius: BorderRadius.zero,
               borderSide: BorderSide(color: AppColors.primary50),
