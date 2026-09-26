438ec73ef6567bfe9598c6c5a4cdb58de74f32d7
Kamran <41294454+kamrans262@users.noreply.github.com>
2026-09-26 20:43:52 +0500

    test(filter): remove top padding from location input [skip ci]

--- lib/features/discovery/presentation/screens/discovery_filter_screen.dart
+++ lib/features/discovery/presentation/screens/discovery_filter_screen.dart
@@ -586,7 +586,7 @@ class _DiscoveryFilterScreenState extends ConsumerState<DiscoveryFilterScreen> {
       suffixIcon: suffixIcon,
       filled: true,
       fillColor: AppColors.filterInputFill,
-      contentPadding: const EdgeInsets.fromLTRB(14, 50, 14, 0),
+      contentPadding: const EdgeInsets.fromLTRB(14, 0, 14, 0),
       enabledBorder: const OutlineInputBorder(
         borderRadius: BorderRadius.zero,
         borderSide: BorderSide(color: AppColors.primary, width: 1),
