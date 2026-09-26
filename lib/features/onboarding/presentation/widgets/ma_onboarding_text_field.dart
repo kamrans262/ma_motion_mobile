72a7a9da17cb0480408e6b6f2c9e32550b62cd91
Kamran <41294454+kamrans262@users.noreply.github.com>
2026-09-26 20:12:53 +0500

    test(ui): remove single-line onboarding input bottom padding [skip ci]

--- lib/features/onboarding/presentation/widgets/ma_onboarding_text_field.dart
+++ lib/features/onboarding/presentation/widgets/ma_onboarding_text_field.dart
@@ -71,7 +71,7 @@ class MaOnboardingTextField extends StatelessWidget {
               20,
               maxLines > 1 ? 21 : 18,
               20,
-              maxLines > 1 ? 11 : 10,
+              maxLines > 1 ? 11 : 0,
             ),
             border: OutlineInputBorder(
               borderRadius: BorderRadius.zero,
