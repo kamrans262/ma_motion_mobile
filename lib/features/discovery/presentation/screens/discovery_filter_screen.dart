410cc6b047e13bde68d97633428ccf9cda236683
Kamran <41294454+kamrans262@users.noreply.github.com>
2026-09-26 20:43:11 +0500

    test(filter): remove textAlignVertical from location input [skip ci]

--- lib/features/discovery/presentation/screens/discovery_filter_screen.dart
+++ lib/features/discovery/presentation/screens/discovery_filter_screen.dart
@@ -445,7 +445,6 @@ class _DiscoveryFilterScreenState extends ConsumerState<DiscoveryFilterScreen> {
           key: const Key('filter_location_search'),
           controller: _locationSearchController,
           onChanged: _searchLocations,
-          textAlignVertical: TextAlignVertical.bottom,
           style: AppTextStyles.field.copyWith(fontSize: 16),
           cursorColor: AppColors.primary,
           decoration: _inputDecoration(
